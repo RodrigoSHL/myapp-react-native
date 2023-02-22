41387703f9eab51d96238807203fef2ee787436f
Rodrigo Catalan Lopez <ext_recatalanl@falabella.cl>
2023-02-22 13:43:04 -0300

    FEAT: work with dimension
    Signed-off-by: Rodrigo Catalan Lopez <ext_recatalanl@falabella.cl>

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {BoxObjectModelScreen} from './src/screen/BoxObjectModelScreen';
+import { DimensionScreen } from './src/screen/DimensionScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      {/* // <HiScreen />
-      // <CounterScreen /> */}
-      <BoxObjectModelScreen />
+      {/* <HiScreen /> */}
+      {/* <CounterScreen />  */}
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionScreen/>
     </SafeAreaView>
   );
 };
---
+++ src/screen/DimensionScreen.tsx
@@ -0,0 +1,39 @@
+import {Dimensions, StyleSheet, Text, useWindowDimensions, View} from 'react-native';
+import React from 'react';
+
+// const {width, height} = Dimensions.get('window');
+
+export const DimensionScreen = () => {
+
+  const {width, height} = useWindowDimensions()
+  return (
+    <View>
+      <View style={styles.container}>
+        <View style={{
+          ...styles.purpleBox,
+          width: width * 1
+          }}/>
+      </View>
+      <Text>width: {width}, height: {height}</Text>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: 200,
+    backgroundColor: 'lightblue'
+  },
+  purpleBox: {
+    backgroundColor: '#581845',
+    width: '50%',
+    height: '50%'
+  },
+  orangeBox: {
+  },
+  title:{
+    fontSize:10
+  }
+});
