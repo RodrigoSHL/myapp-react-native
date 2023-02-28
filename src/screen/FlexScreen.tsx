47ed985b678bbe72bb8806999b8aa963e796fbc5
RodrigoSHL <shl.catalan@gmail.com>
2023-02-27 22:52:54 -0300

    FEAT: work with align items
    Signed-off-by: RodrigoSHL <shl.catalan@gmail.com>

--- src/screen/FlexScreen.tsx
+++ src/screen/FlexScreen.tsx
@@ -1,38 +1,37 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
 
 export default function FlexScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.box1}>FlexScreen</Text>
-      <Text style={styles.box2}>FlexScreen</Text>
-      <Text style={styles.box3}>FlexScreen</Text>
-
+      <Text style={styles.box1}>Box1</Text>
+      <Text style={styles.box2}>Box2</Text>
+      <Text style={styles.box3}>Box3</Text>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: 'lightblue'
-    },
-    box1:{
-        flex:1,
-        borderWidth: 2,
-        borderColor: 'white',
-        fontSize: 20
-    },
-    box2:{
-        flex:2,
-        borderWidth: 2,
-        borderColor: 'white',
-        fontSize: 20
-    },
-    box3:{
-        flex:6,
-        borderWidth: 2,
-        borderColor: 'white',
-        fontSize: 20
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: 'lightblue',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
+  },
+  box1: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 20,
+  },
+  box2: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 20,
+  },
+  box3: {
+    borderWidth: 2,
+    borderColor: 'white',
+    fontSize: 20,
+  },
+});
